b9285279916793e3c25ad26d1b279a817237b133
rin <krin02@naver.com>
2025-06-03 15:15:07 +0900

    chore : Part 엔티티, 리포지터리, 컨트롤러 초기

---
+++ src/main/java/org/jun/project/repository/PartRepository.java
@@ -0,0 +1,8 @@
+package org.jun.project.repository;
+
+import org.jun.project.entity.Part;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PartRepository extends JpaRepository<Part, String> {
+
+}

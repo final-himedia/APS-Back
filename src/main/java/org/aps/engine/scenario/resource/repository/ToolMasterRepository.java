b266485cd1b89ac385db1613ca9ccb57043fa234
kms <komi254@naver.com>
2025-06-10 15:01:12 +0900

    feat:toolmaster작업

---
+++ src/main/java/org/aps/engine/scenario/resource/repository/ToolMasterRepository.java
@@ -0,0 +1,7 @@
+package org.aps.engine.scenario.resource.repository;
+
+import org.aps.engine.scenario.resource.entity.ToolMaster;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ToolMasterRepository extends JpaRepository<ToolMaster, String> {
+}

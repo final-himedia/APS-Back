5d9c67f3003062caef883346595ac853b381f37d
rin <krin02@naver.com>
2025-06-10 11:56:13 +0900

    fix : Priority 엔티티 수정

--- src/main/java/org/aps/engine/scenario/config/entity/PriorityId.java
+++ src/main/java/org/aps/engine/scenario/config/entity/PriorityId.java
@@ -3,13 +3,15 @@ package org.aps.engine.scenario.config.entity;
 import jakarta.persistence.Embeddable;
 import lombok.*;
 
+import java.io.Serializable;
+
 @Embeddable
 @Setter
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class PriorityId {
+public class PriorityId implements Serializable {
     private String scenarioId;
     private String priorityId;
     private String factorId;

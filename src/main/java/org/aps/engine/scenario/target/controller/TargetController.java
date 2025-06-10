5c3a101bf78a9dc66a688edf4e0404099ae77454
rin <krin02@naver.com>
2025-06-10 18:04:44 +0900

    feat : target 모듈정리(엔티티, 리포지, 컨트롤러 수정반영)

--- src/main/java/org/aps/engine/scenario/target/controller/TargetController.java
+++ src/main/java/org/aps/engine/scenario/target/controller/TargetController.java
@@ -1,14 +1,15 @@
 package org.aps.engine.scenario.target.controller;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.aps.engine.scenario.bop.entity.Routing;
+import org.aps.engine.scenario.target.service.DemandService;
 import org.aps.engine.scenario.target.entity.Demand;
 import org.aps.engine.scenario.target.repository.DemandRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -19,7 +20,7 @@ import java.util.Map;
 public class TargetController {
 
     private final DemandRepository demandRepository;
-
+    private final DemandService demandService;
     @GetMapping("/demand")
     public ResponseEntity<?> getAllDemand() {
 
@@ -31,5 +32,16 @@ public class TargetController {
         response.put("total", demands.size());
 
         return ResponseEntity.status(200).body(response);
+    }
+
+    @PostMapping("/demand-upload")
+    public ResponseEntity<String > uploadDemandExcel(@RequestParam("file") MultipartFile file) throws IOException {
+        demandService.excelHandle(file);
+        return ResponseEntity.ok("엑셀 업로드 완료");
+    }
+
+    @GetMapping("/demand-download")
+    private void downloadDemandExcel(HttpServletResponse response) throws IOException {
+        demandService.exportDemandExcel(response);
     }
 }

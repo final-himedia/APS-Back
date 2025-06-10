0f06e5883d06e8ec04a16e56f27e9a2bfb5b8cf5
rin <krin02@naver.com>
2025-06-10 12:49:10 +0900

    fix : 리포지터리 오류 수정

--- src/main/java/org/aps/engine/scenario/bop/repository/DemandRepository.java
+++ src/main/java/org/aps/engine/scenario/bop/repository/DemandRepository.java
@@ -1,8 +1,9 @@
 package org.aps.engine.scenario.bop.repository;
 
-import org.aps.engine.execution.entity.Demand;
+import org.aps.engine.scenario.bop.entity.Demand;
+import org.aps.engine.scenario.bop.entity.DemandId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DemandRepository extends JpaRepository<Demand, Long> {
+public interface DemandRepository extends JpaRepository<Demand, DemandId> {
 
 }
--- src/main/java/org/aps/engine/scenario/bop/repository/OperationRoutingRepository.java
+++ src/main/java/org/aps/engine/scenario/bop/repository/OperationRoutingRepository.java
@@ -1,8 +1,9 @@
 package org.aps.engine.scenario.bop.repository;
 
-import org.aps.engine.execution.entity.OperationRouting;
+import org.aps.engine.scenario.bop.entity.OperationRouting;
+import org.aps.engine.scenario.bop.entity.OperationRoutingId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OperationRoutingRepository extends JpaRepository<OperationRouting, Long> {
+public interface OperationRoutingRepository extends JpaRepository<OperationRouting, OperationRoutingId> {
 
 }

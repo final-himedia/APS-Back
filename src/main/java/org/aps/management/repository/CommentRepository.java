4664f557f05e13e89fe21f3f6a4ed00a96cee9e2
rin <krin02@naver.com>
2025-06-12 11:15:15 +0900

    feat : 문의 게시판 댓글 (등록/수정/삭제) 기능 구현

--- src/main/java/org/aps/management/repository/CommentRepository.java
+++ src/main/java/org/aps/management/repository/CommentRepository.java
@@ -4,13 +4,15 @@ import org.aps.management.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
 
     // 삭제되지 않은 댓글만 조회
     List<Comment> findByQnaIdAndDeletedFalse(Integer qnaId);
 
-    // 특정 게시글에 달린 댓글 조회
-    List<Comment> findByQnaId(Integer qnaId);
+    // 특정 댓글 조회
+    Optional<Comment> findByIdAndQnaIdAndDeletedFalse(Integer commentId, Integer qnaId);
+
 
 }

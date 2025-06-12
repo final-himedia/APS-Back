ac49a766352712face30c59dfaa8926c3cefbd4d
java-enhanced-1 <jchangjun6@gmail.com>
2025-06-12 09:39:48 +0900

    feat : 로그인/로그아웃/비밀번호 찾기/비밀번호 변경

--- src/main/java/org/aps/management/repository/CommentRepository.java
+++ src/main/java/org/aps/management/repository/CommentRepository.java
@@ -2,6 +2,8 @@ package org.aps.management.repository;
 
 import org.aps.management.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
 }

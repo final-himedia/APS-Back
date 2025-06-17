3c7d7758ab8b805ac68142467355dcbe25a2b732
rin <krin02@naver.com>
2025-06-17 12:00:27 +0900

    feat : 사용자 관리 조회 기능

--- src/main/java/org/aps/common/repository/UserRepository.java
+++ src/main/java/org/aps/common/repository/UserRepository.java
@@ -1,14 +1,8 @@
 package org.aps.common.repository;
-
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import org.aps.common.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {

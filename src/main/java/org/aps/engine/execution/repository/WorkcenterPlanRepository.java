45f3adcc92dbadef1ec54656b40f53ce08ab0f71
rin <krin02@naver.com>
2025-06-26 14:49:50 +0900

    feat: 오타 수정

--- src/main/java/org/aps/engine/execution/repository/WorkcenterPlanRepository.java
+++ src/main/java/org/aps/engine/execution/repository/WorkcenterPlanRepository.java
@@ -1,15 +1,15 @@
 package org.aps.engine.execution.repository;
 
 
-import org.aps.engine.result.entity.workcenterPlan;
+import org.aps.engine.result.entity.WorkcenterPlan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface WorkcenterPlanRepository extends JpaRepository<workcenterPlan, Integer > {
+public interface WorkcenterPlanRepository extends JpaRepository<WorkcenterPlan, Integer > {
 
 
-    List<workcenterPlan> findAllByScenarioId(String scenarioId);
+    List<WorkcenterPlan> findAllByScenarioId(String scenarioId);
 }
--- src/main/java/org/aps/engine/result/entity/WorkcenterPlan.java
+++ src/main/java/org/aps/engine/result/entity/WorkcenterPlan.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Entity
 @Table(name = "workcenter_plan")
-public class workcenterPlan {
+public class WorkcenterPlan {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
